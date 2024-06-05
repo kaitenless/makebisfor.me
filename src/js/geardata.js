1f6a024e513174c5e6c2291c682e526d6c65a5c4
kaitenless <github@makebisfor.me>
2024-06-06 00:17:48 +0800

    working on materia representation

---
+++ src/js/geardata.js
@@ -0,0 +1,49 @@
+const weapons=[
+
+    {
+        'gearindex':1,
+        'mainstat':100,
+        'crit':120,
+        'dh':150,
+        'slot1':[1,1,0],
+        'slot2':[1,1,0]
+    },
+    {    
+        'gearindex':2,
+        'mainstat':100,
+        'crit':200,
+        'dh':200,
+        'slot1':'critdh',
+        'slot2':'critdh'
+    }
+
+];
+
+const hats=[
+
+    {
+        'gearindex':1,
+        'mainstat':100,
+        'crit':120,
+        'dh':150,
+        'slot1':'critdh',
+        'slot2':'critdh'
+    },
+    {    
+        'gearindex':2,
+        'mainstat':100,
+        'crit':200,
+        'dh':200,
+        'slot1':'critdh',
+        'slot2':'critdh'
+    }
+
+];
+
+
+
+export function initialisegear(){
+    console.log("hello world");
+    localStorage.setItem('weapons', JSON.stringify(weapons));
+    localStorage.setItem('hats', JSON.stringify(hats));
+}
--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,42 @@ import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+import * as geardata from './geardata.js'
+
+const bigmateria=40;
+const smallmateria=12;
+
+localStorage.clear();
+geardata.initialisegear();
+
+const weaponsdata = JSON.parse(localStorage.getItem('weapons'));
+var weaponsdataperms=[];
+
+for(var i=0;i<weaponsdata.length;i++)
+  {
+    var tempdata=[
+      {    
+        'gearindex': weaponsdata[i].gearindex,
+        'mainstat':weaponsdata[i].mainstat,
+        'crit':weaponsdata[i].crit+bigmateria*weaponsdata[i].slot1[0]
+      }
+    ]
+    console.log(tempdata)
+    weaponsdataperms.concat()
+  }
+
+const hatsdata = JSON.parse(localStorage.getItem('weapons'));
+console.log(hatsdata);
+
+const alldata={
+  weaponsdata,
+  hatsdata
+}
+
+var button = document.getElementById("solvebutton");
+button.addEventListener("click", startsolving);
+
+export function startsolving(){
+  console.log(alldata.weaponsdata[0].slot1);
+}
